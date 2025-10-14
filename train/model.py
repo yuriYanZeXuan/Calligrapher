e6d0fbe9d04740209c63d069dea8bc1b112af9bc
yanzexuan <yanzexuan@xiaohongshu.com>
2025-10-14 15:27:35 +0800

    fix

--- train/model.py
+++ train/model.py
@@ -79,6 +79,7 @@ class MLPProjModel(nn.Module):
     def __init__(self, cross_attention_dim=768, id_embeddings_dim=512, num_tokens=4):
         super().__init__()
         self.num_tokens = num_tokens
+        self.cross_attention_dim = cross_attention_dim
         self.proj = nn.Sequential(
             nn.Linear(id_embeddings_dim, id_embeddings_dim * 2),
             nn.GELU(),

26533bd3b30bceda5cc25d27f98526dcd56999cf
yanzexuan <yanzexuan@xiaohongshu.com>
2025-10-15 18:34:33 +0800

    fix

--- train/rl_ip/ocr.py
+++ train/rl_ip/ocr.py
@@ -7,11 +7,9 @@ class OCRScorer:
         # Initialize PaddleOCR based on the reference script to disable unnecessary modules.
         # 'ch' lang model supports both Chinese and English.
         self.ocr_model = PaddleOCR(
-            use_doc_orientation_classify=False,
-            use_doc_unwarping=False,
-            use_textline_orientation=False,
-            use_angle_cls=True, 
-            lang='ch'
+            use_doc_orientation_classify=False, # 通过 use_doc_orientation_classify 参数指定不使用文档方向分类模型
+            use_doc_unwarping=False, # 通过 use_doc_unwarping 参数指定不使用文本图像矫正模型
+            use_textline_orientation=False, # 通过 use_textline_orientation 参数指定不使用文本行方向分类模型
         )
         print("Initialized PaddleOCR Model.")
 
